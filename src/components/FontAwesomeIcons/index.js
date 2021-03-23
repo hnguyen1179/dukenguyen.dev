fa0e941ce2152d0eebc89fd0609a6eb064edb29c
Duke <hnguyen1179@berkeley.edu>
2021-03-23 16:59:35 -0700

    forgot a file;

--- src/components/FontAwesomeIcons/index.js
+++
@@ -1,5 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-
-library.add(fab, faEnvelope)
