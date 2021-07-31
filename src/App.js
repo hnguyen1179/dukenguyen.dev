968d6031216cfaeb27441049053f452561b5a990
Duke <hnguyen1179@berkeley.edu>
2021-07-30 21:04:06 -0700

    Added initial page load animation

--- src/App.js
+++ src/App.js
@@ -1,68 +1,71 @@
-import React from 'react';
-import { Switch, Route, useLocation, Redirect } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import React from "react";
+import { Switch, Route, useLocation, Redirect } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import Hidden from './pages/Hidden';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Hidden from "./pages/Hidden";
 
-import Header from './components/Header';
+import Header from "./components/Header";
 
-import PortfolioContext from './context/context';
-import { 
-  headerData, 
-  titleData,
-  homeData,
-  aboutData, 
-  projectsData, 
-  contactData, 
-  footerData 
-} from './mock/data'; 
+import PortfolioContext from "./context/context";
+import {
+	headerData,
+	titleData,
+	homeData,
+	aboutData,
+	projectsData,
+	contactData,
+	footerData,
+} from "./mock/data";
 
-import './stylesheets/main.scss';
+import "./stylesheets/main.scss";
 
 export default function App() {
-  const location = useLocation();
+	const location = useLocation();
 
-  return (
-    <PortfolioContext.Provider 
-      value={{ 
-        headerData,
-        titleData, 
-        homeData, 
-        aboutData, 
-        projectsData, 
-        contactData, 
-        footerData,
-      }} 
-    >
-      <div className="App">
-        <div className="App__bar App__bar--top" />
-        <div className="App__bar App__bar--bottom" />
-        <div className="App__bar App__bar--left" />
+	window.onbeforeunload = function () {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	};
 
-        <Header />
+	return (
+		<PortfolioContext.Provider
+			value={{
+				headerData,
+				titleData,
+				homeData,
+				aboutData,
+				projectsData,
+				contactData,
+				footerData,
+			}}
+		>
+			<div className="App">
+				<div className="App__bar App__bar--top" />
+				<div className="App__bar App__bar--bottom" />
+				<div className="App__bar App__bar--left" />
 
-        <TransitionGroup component={null}>
-          <CSSTransition
-            key={location.key}
-            timeout={600}
-            classNames="fade"
-          >
-            <Switch location={location}>
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/projects" component={Projects} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/hidden" component={Hidden} />
-              <Route render={() => <Redirect to="/" />} />
-            </Switch>
-          </CSSTransition>
-        </TransitionGroup>
-      </div>
-    </PortfolioContext.Provider>
-  )
+				<Header />
+
+				<TransitionGroup component={null}>
+					<CSSTransition
+						key={location.key}
+						timeout={600}
+						classNames="fade"
+					>
+						<Switch location={location}>
+							<Route exact path="/" component={Home} />
+							<Route path="/about" component={About} />
+							<Route path="/projects" component={Projects} />
+							<Route path="/contact" component={Contact} />
+							<Route path="/hidden" component={Hidden} />
+							<Route render={() => <Redirect to="/" />} />
+						</Switch>
+					</CSSTransition>
+				</TransitionGroup>
+			</div>
+		</PortfolioContext.Provider>
+	);
 }
-
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,73 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { motion } from "framer-motion";
 import Footer from "../components/Footer";
 import PortfolioContext from "../context/context";
 
 export default function Home() {
-    const { homeData } = useContext(PortfolioContext);
+	const { homeData } = useContext(PortfolioContext);
+	const introEffect = "initialLoad" in sessionStorage ? false : true;
 
-    return (
-        <div className="Home page">
-            <div className="Home__title">
-                <div className="Home__title__container">
-                    <img
-                        draggable={false}
-                        className="svg"
-                        alt="Duke Nguyen"
-                        src={homeData.images.nameSvg}
-                    />
-                    <div className="image-container">
-                        <img
-                            src={homeData.images.portrait}
-                            alt="Duke Nguyen Portrait"
-                        />
-                    </div>
-                </div>
-            </div>
-            <main className="Home__main page__content">
-                <h1 style={{ display: "none" }}>Duke Nguyen</h1>
-                <section>
-                    {homeData.paragraphs.map((paragraph) => (
-                        <p className="Home__main__paragraph" key={paragraph[5]}>
-                            {paragraph}
-                        </p>
-                    ))}
-                </section>
-            </main>
-            <Footer />
-        </div>
-    );
+	const transition = { duration: 1.1, ease: "easeInOut" };
+
+	useEffect(() => {
+		if (!("initialLoad" in sessionStorage)) {
+			sessionStorage.setItem("initialLoad", true);
+		}
+	}, []);
+
+	return (
+		<motion.div
+			className="Home page"
+		>
+			<motion.div
+				initial={{ y: "110%" }}
+				animate={{ y: "0%" }}
+				transition={introEffect ? transition : { duration: 0 }}
+				// transition={transition}
+				className="Home__title"
+			>
+				<div className="Home__title__container">
+					<img
+						draggable={false}
+						className="svg"
+						alt="Duke Nguyen"
+						src={homeData.images.nameSvg}
+					/>
+					<div className="image-container">
+						<img
+							src={homeData.images.portrait}
+							alt="Duke Nguyen Portrait"
+						/>
+					</div>
+				</div>
+			</motion.div>
+			<motion.main
+				initial={{
+					color: introEffect ? "#e9dfd8" : "#af4818",
+					// color: "#e9dfd8",
+					backgroundColor: "#e9dfd8",
+					zIndex: 30,
+				}}
+				animate={{
+					color: "#af4818",
+					zIndex: 0,
+					transitionDelay: "1s",
+					transitionDuration: "0.5s",
+					// transitionDuration: introEffect ? "0.5s" : 0,
+					// transitionDelay: introEffect ? "0.85s" : 0,
+				}}
+				className="Home__main page__content"
+			>
+				<h1 style={{ display: "none" }}>Duke Nguyen</h1>
+				<section>
+					{homeData.paragraphs.map((paragraph) => (
+						<p className="Home__main__paragraph" key={paragraph[5]}>
+							{paragraph}
+						</p>
+					))}
+				</section>
+			</motion.main>
+			<Footer />
+		</motion.div>
+	);
 }
