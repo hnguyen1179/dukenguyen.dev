e1e8d469784f1eb484b6a437ea4262532d03b290
Duke <hnguyen1179@berkeley.edu>
2021-10-14 12:38:29 -0700

    Fixed up the UX; added frosting to the link menu and it also closes on scroll

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,77 +1,84 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { NavLink } from 'react-router-dom';
-import Hamburger from './Hamburger';
+import { NavLink } from "react-router-dom";
+import Hamburger from "./Hamburger";
 
 export default function Navigation({ headerData }) {
-  const [showNav, setShowNav ] = useState(false);
+	const [showNav, setShowNav] = useState(false);
 
-  const handlePreventBodyClick = (e) => {
-    e.stopPropagation();
-  }
+	const handlePreventBodyClick = (e) => {
+		e.stopPropagation();
+	};
 
-  const handleClickLink = (e) => {
-    e.stopPropagation();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
+	const handleClickLink = (e) => {
+		e.stopPropagation();
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	};
 
-  const handleBodyClick = (e) => {
-    const closeNavigation = new Event('closeNavigation');
-    window.dispatchEvent(closeNavigation);
-  }
+	const handleBodyClick = (e) => {
+		const closeNavigation = new Event("closeNavigation");
+		window.dispatchEvent(closeNavigation);
+	};
 
-  const handleCloseNavigation = (e) => {
-    setShowNav(false);
-  }
+	const handleCloseNavigation = (e) => {
+		if (showNav === true) setShowNav(false);
+	};
 
-  const handleOpenNavigation = (e) => {
-    e.stopPropagation();
-    setShowNav(!showNav);
-  }
+	const handleOpenNavigation = (e) => {
+		e.stopPropagation();
+		setShowNav(!showNav);
+	};
 
-  useEffect(() => {
-    window.addEventListener('closeNavigation', handleCloseNavigation);
-    document.body.addEventListener('click', handleBodyClick)
+	useEffect(() => {
+		window.addEventListener("closeNavigation", handleCloseNavigation);
+		window.addEventListener("scroll", handleCloseNavigation);
+		document.body.addEventListener("click", handleBodyClick);
 
-    return () => {
-      window.removeEventListener('closeNavigation', handleCloseNavigation);
-      document.body.removeEventListener('click', handleBodyClick)
-    }
-  }, [])
+		return () => {
+			window.removeEventListener(
+				"closeNavigation",
+				handleCloseNavigation
+			);
+			window.removeEventListener("scroll", handleCloseNavigation);
+			document.body.removeEventListener("click", handleBodyClick);
+		};
+	}, []);
 
-  const renderLinks = (
-    headerData.links.map((link) => (
-      <NavLink 
-        key={link.id}
-        exact to={link.path} 
-        className="link" 
-        activeClassName="activeLink"
-        onClick={handleClickLink}
-      >
-        {link.name[0].toUpperCase() + link.name.slice(1)}
-      </NavLink>
-    ))
-  )
+	const renderLinks = headerData.links.map((link) => (
+		<NavLink
+			key={link.id}
+			exact
+			to={link.path}
+			className="link"
+			activeClassName="activeLink"
+			onClick={handleClickLink}
+		>
+			{link.name[0].toUpperCase() + link.name.slice(1)}
+		</NavLink>
+	));
 
-  return (
-    <div className="Header__navigation">
-      <nav 
-        className={`Header__navigation__links ${showNav && 'show'}`} 
-        onClick={handlePreventBodyClick}
-      >
-        <div className="overflow-container">
-          {renderLinks}
-        </div>
-      </nav>
-      <nav 
-        className={`Header__navigation__links--sidebar ${showNav && 'show'}`} 
-        onClick={handlePreventBodyClick}
-      >
-        <div className="Header__navigation__links--sidebar__container">
-          {renderLinks}
-        </div>
-      </nav>
-      <Hamburger showNav={showNav} handleOpenNavigation={handleOpenNavigation} />
-    </div>
-  )
+	return (
+		<div className="Header__navigation">
+			<nav
+				className={`Header__navigation__links ${showNav && "show"}`}
+				onClick={handlePreventBodyClick}
+			>
+				<div className="overflow-container">{renderLinks}</div>
+			</nav>
+			<nav
+				className={`Header__navigation__links--sidebar ${
+					showNav && "show"
+				}`}
+				onClick={handlePreventBodyClick}
+			>
+				<div className="Header__navigation__links--sidebar__container">
+					{renderLinks}
+				</div>
+			</nav>
+			<Hamburger
+				showNav={showNav}
+				handleOpenNavigation={handleOpenNavigation}
+			/>
+		</div>
+	);
 }
