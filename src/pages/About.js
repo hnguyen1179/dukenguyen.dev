ce6c80f9a60309cd36f3e06ae70d1424df1d39f3
Duke <hnguyen1179@berkeley.edu>
2023-04-27 18:01:26 -0700

    Remove spotify API

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,93 +1,14 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import PortfolioContext from "../context/context";
 import Title from "../components/Title";
 import Footer from "../components/Footer";
-import APPController from "../utility/spotifyApi";
 
 export default function About() {
 	const { aboutData } = useContext(PortfolioContext);
-	const [tracks, setTracks] = useState([]);
-	const [artists, setArtists] = useState("uhhhhh");
-	const [albumCovers, setAlbumCovers] = useState([]);
-
-	useEffect(() => {
-		try {
-			const getData = async () => {
-				const [topTracks, topArtists] = await APPController.getData();
-
-				// Setting up top artists
-				const topArtistNames = topArtists.map((obj) => obj.name);
-				topArtistNames[3] = "and " + topArtistNames[3] + ".";
-				const formattedArtists = topArtistNames.join(", ");
-				setArtists(formattedArtists);
-
-				// Setting up top tracks
-				const randomFourTracks = [];
-				while (randomFourTracks.length < 4) {
-					const index = Math.floor(Math.random() * 50);
-					if (randomFourTracks.includes(index)) continue;
-					randomFourTracks.push(index);
-				}
-
-				setTracks(topTracks);
-				setAlbumCovers(randomFourTracks.map((idx) => topTracks[idx]));
-			};
-
-			getData();
-		} catch (e) {
-			setArtists(
-				"uhhh, I can't think right now. (My code's erroring out! I'll fix this soon)"
-			);
-		}
-	}, []);
-
-	const resetAlbumCovers = () => {
-		// Setting up top tracks
-		const randomFourTracks = new Set();
-		while (randomFourTracks.size < 4) {
-			const index = Math.floor(Math.random() * 50);
-
-			randomFourTracks.add(index);
-		}
-
-		setAlbumCovers(
-			[...randomFourTracks].map((idx) => tracks[idx]).slice(0, 4)
-		);
-	};
 
 	const renderOutdoors = aboutData.images.paragraphOne.map((image) => (
 		<img key={image.alt} src={image.url} alt={image.alt} />
 	));
-
-	const addNBSP = (str) => {
-		const arrayForm = str.split(" ");
-		return arrayForm.reduce((acc, cv, idx) => {
-			if (idx === arrayForm.length - 1) {
-				return acc + "\u00a0" + cv;
-			}
-			return acc + " " + cv;
-		}, "");
-	};
-
-	const renderAlbumCovers = albumCovers.map((track) => {
-		return (
-			<a
-				href={track.external_urls.spotify}
-				target="_blank"
-				key={track.album.name + " " + track.name}
-				rel="noreferrer"
-			>
-				<img
-					src={track.album.images[0].url}
-					alt={`${track.album.name}'s Album Cover. Track name is ${track.name}`}
-				/>
-				<div className="track-info">
-					<div>{addNBSP(track.name)}</div>
-					<div>{`${addNBSP(track.artists[0].name)}`}</div>
-				</div>
-			</a>
-		);
-	});
 
 	const renderBooksAndMovies = aboutData.images.paragraphThree.map(
 		(image) => <img key={image.alt} src={image.url} alt={image.alt} />
@@ -107,25 +28,13 @@ export default function About() {
 								key={paragraph.length}
 								className={`About__main__paragraphs__content__paragraph About__main__paragraphs__content__paragraph--${idx}`}
 								dangerouslySetInnerHTML={{
-									__html: paragraph.replace(
-										/\{artists\}/,
-										artists
-									),
+									__html: paragraph
 								}}
 							/>
-							{idx === 2 ? (
-								<button
-									className="more-songs-button"
-									onClick={resetAlbumCovers}
-								>
-									<span>Randomize songs</span>
-								</button>
-							) : null}
 							<div
 								className={`About__main__paragraphs__content__media About__main__paragraphs__content__media--${idx}`}
 							>
 								{idx === 1 ? renderOutdoors : null}
-								{idx === 2 ? renderAlbumCovers : null}
 								{idx === 3 ? renderBooksAndMovies : null}
 							</div>
 						</div>
--- src/utility/spotifyApi.js
+++ src/utility/spotifyApi.js
@@ -8,81 +8,81 @@ const ACCESS_TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
 const BASIC = "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET);
 
 const TOP_TRACKS_ENDPOINT =
-	"https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50";
+  "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50";
 const TOP_ARTISTS_ENDPOINT =
-	"https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=4";
+  "https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=4";
 
 const APIController = (function () {
-	// Private methods
-	const _getAccessToken = async () => {
-		const response = await fetch(ACCESS_TOKEN_ENDPOINT, {
-			method: "POST",
-			headers: {
-				Authorization: BASIC,
-				"Content-Type": "application/x-www-form-urlencoded",
-			},
-			body: querystring.stringify({
-				grant_type: "refresh_token",
-				refresh_token: REFRESH_TOKEN,
-			}),
-		});
+  // Private methods
+  const _getAccessToken = async () => {
+    const response = await fetch(ACCESS_TOKEN_ENDPOINT, {
+      method: "POST",
+      headers: {
+        Authorization: BASIC,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: querystring.stringify({
+        grant_type: "refresh_token",
+        refresh_token: REFRESH_TOKEN,
+      }),
+    });
 
-		return response.json();
-	};
+    return response.json();
+  };
 
-	const _getTopTracks = async (accessToken) => {
-		const response = await fetch(TOP_TRACKS_ENDPOINT, {
-			headers: {
-				Authorization: `Bearer ${accessToken}`,
-			},
-		});
+  const _getTopTracks = async (accessToken) => {
+    const response = await fetch(TOP_TRACKS_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
 
-		return response.json();
-	};
+    return response.json();
+  };
 
-	const _getTopArtists = async (accessToken) => {
-		const response = await fetch(TOP_ARTISTS_ENDPOINT, {
-			headers: {
-				Authorization: `Bearer ${accessToken}`,
-			},
-		});
+  const _getTopArtists = async (accessToken) => {
+    const response = await fetch(TOP_ARTISTS_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
 
-		return response.json();
-	};
+    return response.json();
+  };
 
-	return {
-		getAccessToken() {
-			return _getAccessToken();
-		},
-		getTopTracks(accessToken) {
-			return _getTopTracks(accessToken);
-		},
-		getTopArtists(accessToken) {
-			return _getTopArtists(accessToken);
-		},
-	};
+  return {
+    getAccessToken() {
+      return _getAccessToken();
+    },
+    getTopTracks(accessToken) {
+      return _getTopTracks(accessToken);
+    },
+    getTopArtists(accessToken) {
+      return _getTopArtists(accessToken);
+    },
+  };
 })();
 
 const APPController = (function (APICtrl) {
-	const getSpotifyData = async () => {
-		const { access_token: accessToken } = await APICtrl.getAccessToken();
-		const { items: topTracks } = await APICtrl.getTopTracks(accessToken);
-		const { items: topArtists } = await APICtrl.getTopArtists(accessToken);
+  const getSpotifyData = async () => {
+    const { access_token: accessToken } = await APICtrl.getAccessToken();
+    const { items: topTracks } = await APICtrl.getTopTracks(accessToken);
+    const { items: topArtists } = await APICtrl.getTopArtists(accessToken);
 
-		// Artist with explicit album covers
-		const bannedArtists = ["Teyana Taylor", "Blood Orange"];
-		const filteredTracks = topTracks.filter(
-			(track) => !bannedArtists.includes(track.artists[0].name)
-		);
+    // Artist with explicit album covers
+    const bannedArtists = ["Teyana Taylor", "Blood Orange"];
+    const filteredTracks = topTracks.filter(
+      (track) => !bannedArtists.includes(track.artists[0].name)
+    );
 
-		return [filteredTracks, topArtists];
-	};
+    return [filteredTracks, topArtists];
+  };
 
-	return {
-		getData() {
-			return getSpotifyData();
-		},
-	};
+  return {
+    getData() {
+      return getSpotifyData();
+    },
+  };
 })(APIController);
 
 export default APPController;
